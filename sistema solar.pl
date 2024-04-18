2cdb11baca8d2ff2d6b915a8a511521da32ecfb9
Rimskiy13 <romanch422@gmail.com>
2024-04-18 10:38:55 -0600

    base de conocimiento del sistema solar

---
+++ sistema solar.pl
@@ -0,0 +1,75 @@
+planeta(mercurio).
+planeta(venus).
+planeta(tierra).
+planeta(marte).
+planeta(jupiter).
+planeta(saturno).
+planeta(urano).
+planeta(neptuno).
+
+satelite(fobos).
+satelite(deimos).
+satelite(io).
+satelite(europa).
+satelite(ganimedes).
+satelite(calisto).
+satelite(titan).
+satelite(encelado).
+satelite(mimas).
+satelite(tethys).
+satelite(dione).
+satelite(rhea).
+satelite(titania).
+satelite(oberon).
+satelite(umbriel).
+satelite(ariel).
+satelite(miranda).
+satelite(triton).
+satelite(nereida).
+satelite(proteo).
+
+orbita_alrededor_de(luna, tierra).
+orbita_alrededor_de(fobos, marte).
+orbita_alrededor_de(deimos, marte).
+orbita_alrededor_de(io, jupiter).
+orbita_alrededor_de(europa, jupiter).
+orbita_alrededor_de(ganimedes, jupiter).
+orbita_alrededor_de(calisto, jupiter).
+orbita_alrededor_de(titan, saturno).
+orbita_alrededor_de(encelado, saturno).
+orbita_alrededor_de(mimas, saturno).
+orbita_alrededor_de(tethys, saturno).
+orbita_alrededor_de(dione, saturno).
+orbita_alrededor_de(rhea, saturno).
+orbita_alrededor_de(titania, urano).
+orbita_alrededor_de(oberon, urano).
+orbita_alrededor_de(umbriel, urano).
+orbita_alrededor_de(ariel, urano).
+orbita_alrededor_de(miranda, urano).
+orbita_alrededor_de(triton, neptuno).
+orbita_alrededor_de(nereida, neptuno).
+orbita_alrededor_de(proteo, neptuno).
+
+tamaño_satelites(luna, 1737).
+tamaño_satelites(fobos, 22.2).
+tamaño_satelites(deimos, 12.4).
+tamaño_satelites(io, 1821.6).
+tamaño_satelites(europa, 1560.8).
+tamaño_satelites(ganimedes, 2634.1).
+tamaño_satelites(calisto, 2410.3).
+tamaño_satelites(titan, 2575).
+tamaño_satelites(encelado, 499.6).
+tamaño_satelites(mimas, 396.4).
+tamaño_satelites(tethys, 1062.).
+tamaño_satelites(dione, 1122.8).
+tamaño_satelites(rhea, 1527.).
+tamaño_satelites(titania, 1578).
+tamaño_satelites(oberon, 1523).
+tamaño_satelites(umbriel, 1169).
+tamaño_satelites(ariel, 1158).
+tamaño_satelites(miranda, 471.6).
+tamaño_satelites(triton, 1353.4).
+tamaño_satelites(nereida, 170).
+tamaño_satelites(proteo, 210).
+
+%hacer regla que si le paso el numero de satelites determine que planeta es
